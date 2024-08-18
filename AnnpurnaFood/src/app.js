042b493109e54ba3e0ae425c571771662f292d19
k-ravi24 <k.ravi19777@gmail.com>
2024-08-18 23:17:05 +0530

    created: restaurant model

--- AnnpurnaFood/src/app.js
+++ AnnpurnaFood/src/app.js
@@ -3,6 +3,13 @@ import restaurantRouter from "./routes/restaurant.js";
 
 const app = express();
 
+// middlewares
+app.use(
+    express.json({
+        limit: "10000",
+    })
+);
+
 // Restaurant Routes
 app.use("/api/v1/restaurant", restaurantRouter);
 
---
+++ AnnpurnaFood/src/models/restaurant.js
@@ -0,0 +1,38 @@
+import { Schema } from "mongoose";
+
+// restaurant schema
+const restaurantSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        cuisines: [{ type: String, required: true }],
+        type: {
+            type: String,
+            enum: [
+                { name: "Vegan", color: ["white", "green", "blue"] },
+                { name: "Vegetarian", color: ["green"] },
+            ],
+            require: true,
+        },
+        address: {
+            locality: { type: String },
+            areaName: { type: String, required: true },
+            city: { type: String, required: true },
+            state: { type: String, required: true },
+            country: { type: String, default: "India" },
+            pincode: { type: Number, required: true },
+        },
+        averageDeliveryTime: { type: Number, required: true },
+        menu: [{ type: Schema.Types.ObjectId, ref: "Menu" }],
+        rating: { type: Schema.Types.ObjectId, ref: "Rating" },
+        review: { type: Schema.Types.ObjectId, ref: "Review" },
+        category: { type: Schema.Types.ObjectId, ref: "Category" },
+        thumbnail: [{ type: String }],
+        isPromoted: { type: Boolean, default: false },
+        gallery: [{ type: String }],
+    },
+    { timestamp: true }
+);
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+
+export default Restaurant;
